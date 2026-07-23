214432247f1a723f7f1a32f927de9d3003a54128
Sriharshareddy6464 <adapalasriharshareddy@gmail.com>
2026-07-23 13:13:21 +0530

    feat(aws): implement PollResource polling waiter helper

--- aws/waiters.go
+++ aws/waiters.go
@@ -2,11 +2,31 @@ package aws
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
 // PollResource repeatedly checks condition until deleted or timeout
 func PollResource(ctx context.Context, checkFunc func() (bool, error), interval, timeout time.Duration) error {
-	// Polling loop logic with timeout and checkFunc
-	return nil
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-timer.C:
+			return fmt.Errorf("timeout waiting for resource status change")
+		case <-ticker.C:
+			deleted, err := checkFunc()
+			if err != nil {
+				return err
+			}
+			if deleted {
+				return nil
+			}
+		}
+	}
 }
