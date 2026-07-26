b6d7db5eeb8f2b24c94611a322b3504e2fab17cc
Sriharshareddy6464 <adapalasriharshareddy@gmail.com>
2026-07-26 13:11:56 +0530

    feat(result): add PendingResources to track propagating distributions

--- models/result.go
+++ models/result.go
@@ -3,4 +3,5 @@ package models
 type Result struct {
 	DeletedResources []Resource `json:"deleted_resources"`
 	FailedResources  []Resource `json:"failed_resources"`
+	PendingResources []Resource `json:"pending_resources,omitempty"`
 }
